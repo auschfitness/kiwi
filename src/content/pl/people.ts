6d25cd067aef534ba767ab0fa56924ca2a728e83
Lucas <lucasjedbarbosa@gmail.com>
2026-09-04 11:25:56 -0300

    feat(pl): family & people deck

---
+++ src/content/pl/people.ts
@@ -0,0 +1,25 @@
+import type { Deck } from '../../types'
+
+export const PL_PEOPLE_DECK: Deck = {
+  id: 'pl_people',
+  name: 'Família e pessoas',
+  emoji: '👪',
+  desc: 'Falar da família e das pessoas ao redor',
+  level: 1,
+  cards: [
+    { id: 'pl_people_0', deckId: 'pl_people', en: 'rodzina', pt: 'família', exampleHtml: 'Moja <b>rodzina</b> jest duża.', examplePt: 'Minha família é grande.', pos: 'noun' },
+    { id: 'pl_people_1', deckId: 'pl_people', en: 'mama', pt: 'mãe', exampleHtml: 'To jest moja <b>mama</b>.', examplePt: 'Esta é a minha mãe.', pos: 'noun' },
+    { id: 'pl_people_2', deckId: 'pl_people', en: 'tata', pt: 'pai', exampleHtml: 'To jest mój <b>tata</b>.', examplePt: 'Este é o meu pai.', pos: 'noun' },
+    { id: 'pl_people_3', deckId: 'pl_people', en: 'brat', pt: 'irmão', exampleHtml: 'Mam jednego <b>brata</b>.', examplePt: 'Eu tenho um irmão.', pos: 'noun' },
+    { id: 'pl_people_4', deckId: 'pl_people', en: 'siostra', pt: 'irmã', exampleHtml: 'Moja <b>siostra</b> mieszka w Warszawie.', examplePt: 'Minha irmã mora em Varsóvia.', pos: 'noun' },
+    { id: 'pl_people_5', deckId: 'pl_people', en: 'syn', pt: 'filho', exampleHtml: 'Mam <b>syna</b>.', examplePt: 'Eu tenho um filho.', pos: 'noun' },
+    { id: 'pl_people_6', deckId: 'pl_people', en: 'córka', pt: 'filha', exampleHtml: 'Moja <b>córka</b> ma pięć lat.', examplePt: 'Minha filha tem cinco anos.', pos: 'noun' },
+    { id: 'pl_people_7', deckId: 'pl_people', en: 'mąż', pt: 'marido', exampleHtml: 'Mój <b>mąż</b> gotuje kolację.', examplePt: 'Meu marido está cozinhando o jantar.', pos: 'noun' },
+    { id: 'pl_people_8', deckId: 'pl_people', en: 'żona', pt: 'esposa', exampleHtml: 'Jego <b>żona</b> jest lekarką.', examplePt: 'A esposa dele é médica.', pos: 'noun' },
+    { id: 'pl_people_9', deckId: 'pl_people', en: 'dziecko', pt: 'criança', exampleHtml: 'To <b>dziecko</b> jest bardzo grzeczne.', examplePt: 'Essa criança é muito educada.', pos: 'noun' },
+    { id: 'pl_people_10', deckId: 'pl_people', en: 'przyjaciel', pt: 'amigo', exampleHtml: 'Mój <b>przyjaciel</b> mieszka blisko.', examplePt: 'Meu amigo mora perto.', pos: 'noun' },
+    { id: 'pl_people_11', deckId: 'pl_people', en: 'dziewczyna', pt: 'namorada / menina', exampleHtml: 'Ta <b>dziewczyna</b> jest miła.', examplePt: 'Essa menina é gentil.', pos: 'noun' },
+    { id: 'pl_people_12', deckId: 'pl_people', en: 'chłopak', pt: 'namorado / menino', exampleHtml: 'Ten <b>chłopak</b> jest wysoki.', examplePt: 'Esse menino é alto.', pos: 'noun' },
+    { id: 'pl_people_13', deckId: 'pl_people', en: 'człowiek', pt: 'pessoa', exampleHtml: 'To jest dobry <b>człowiek</b>.', examplePt: 'Essa é uma boa pessoa.', pos: 'noun' },
+  ],
+}
