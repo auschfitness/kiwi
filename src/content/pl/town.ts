2dd684a09bccf7d3633603bc110014044b8c1cfa
Lucas <lucasjedbarbosa@gmail.com>
2026-09-08 10:04:23 -0300

    feat(pl): town deck (A2)

---
+++ src/content/pl/town.ts
@@ -0,0 +1,25 @@
+import type { Deck } from '../../types'
+
+export const PL_TOWN_DECK: Deck = {
+  id: 'pl_town',
+  name: 'Cidade',
+  emoji: '🏙️',
+  desc: 'Lugares e transporte pela cidade',
+  level: 1,
+  cards: [
+    { id: 'pl_town_0', deckId: 'pl_town', en: 'miasto', pt: 'cidade', exampleHtml: 'To jest moje <b>miasto</b>.', examplePt: 'Essa é minha cidade.', pos: 'noun' },
+    { id: 'pl_town_1', deckId: 'pl_town', en: 'ulica', pt: 'rua', exampleHtml: 'To jest moja <b>ulica</b>.', examplePt: 'Essa é minha rua.', pos: 'noun' },
+    { id: 'pl_town_2', deckId: 'pl_town', en: 'plac', pt: 'praça', exampleHtml: 'To jest <b>plac</b>.', examplePt: 'Essa é a praça.', pos: 'noun' },
+    { id: 'pl_town_3', deckId: 'pl_town', en: 'kościół', pt: 'igreja', exampleHtml: 'To jest <b>kościół</b>.', examplePt: 'Essa é a igreja.', pos: 'noun' },
+    { id: 'pl_town_4', deckId: 'pl_town', en: 'szkoła', pt: 'escola', exampleHtml: 'Moje dzieci idą do <b>szkoły</b>.', examplePt: 'Meus filhos vão à escola.', pos: 'noun' },
+    { id: 'pl_town_5', deckId: 'pl_town', en: 'bank', pt: 'banco', exampleHtml: 'Idę do <b>banku</b>.', examplePt: 'Eu vou ao banco.', pos: 'noun' },
+    { id: 'pl_town_6', deckId: 'pl_town', en: 'poczta', pt: 'correio', exampleHtml: 'Idę na <b>pocztę</b>.', examplePt: 'Eu vou ao correio.', pos: 'noun' },
+    { id: 'pl_town_7', deckId: 'pl_town', en: 'park', pt: 'parque', exampleHtml: 'Idziemy do <b>parku</b>.', examplePt: 'Nós vamos ao parque.', pos: 'noun' },
+    { id: 'pl_town_8', deckId: 'pl_town', en: 'most', pt: 'ponte', exampleHtml: 'To jest <b>most</b>.', examplePt: 'Essa é a ponte.', pos: 'noun' },
+    { id: 'pl_town_9', deckId: 'pl_town', en: 'rzeka', pt: 'rio', exampleHtml: 'To jest <b>rzeka</b>.', examplePt: 'Esse é o rio.', pos: 'noun' },
+    { id: 'pl_town_10', deckId: 'pl_town', en: 'autobus', pt: 'ônibus', exampleHtml: 'To jest <b>autobus</b>.', examplePt: 'Esse é o ônibus.', pos: 'noun' },
+    { id: 'pl_town_11', deckId: 'pl_town', en: 'pociąg', pt: 'trem', exampleHtml: 'To jest <b>pociąg</b>.', examplePt: 'Esse é o trem.', pos: 'noun' },
+    { id: 'pl_town_12', deckId: 'pl_town', en: 'lotnisko', pt: 'aeroporto', exampleHtml: 'Jedziemy na <b>lotnisko</b>.', examplePt: 'Nós vamos ao aeroporto.', pos: 'noun' },
+    { id: 'pl_town_13', deckId: 'pl_town', en: 'dworzec', pt: 'estação', exampleHtml: 'To jest <b>dworzec</b>.', examplePt: 'Essa é a estação.', pos: 'noun' },
+  ],
+}
