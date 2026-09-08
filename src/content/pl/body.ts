d991d66031df99041b3171a373ad5c060cb33368
Lucas <lucasjedbarbosa@gmail.com>
2026-09-08 10:01:33 -0300

    feat(pl): body deck (A2)

---
+++ src/content/pl/body.ts
@@ -0,0 +1,25 @@
+import type { Deck } from '../../types'
+
+export const PL_BODY_DECK: Deck = {
+  id: 'pl_body',
+  name: 'Corpo',
+  emoji: '🧍',
+  desc: 'Partes do corpo, dizer onde dói',
+  level: 1,
+  cards: [
+    { id: 'pl_body_0', deckId: 'pl_body', en: 'głowa', pt: 'cabeça', exampleHtml: 'Boli mnie <b>głowa</b>.', examplePt: 'Minha cabeça dói.', pos: 'noun' },
+    { id: 'pl_body_1', deckId: 'pl_body', en: 'ręka', pt: 'mão / braço', exampleHtml: 'To jest moja <b>ręka</b>.', examplePt: 'Essa é minha mão.', pos: 'noun' },
+    { id: 'pl_body_2', deckId: 'pl_body', en: 'noga', pt: 'perna', exampleHtml: 'To jest moja <b>noga</b>.', examplePt: 'Essa é minha perna.', pos: 'noun' },
+    { id: 'pl_body_3', deckId: 'pl_body', en: 'oko', pt: 'olho', exampleHtml: 'To jest moje <b>oko</b>.', examplePt: 'Esse é meu olho.', pos: 'noun' },
+    { id: 'pl_body_4', deckId: 'pl_body', en: 'ucho', pt: 'orelha', exampleHtml: 'To jest moje <b>ucho</b>.', examplePt: 'Essa é minha orelha.', pos: 'noun' },
+    { id: 'pl_body_5', deckId: 'pl_body', en: 'usta', pt: 'boca', exampleHtml: 'Otwórz <b>usta</b>.', examplePt: 'Abra a boca.', pos: 'noun' },
+    { id: 'pl_body_6', deckId: 'pl_body', en: 'nos', pt: 'nariz', exampleHtml: 'To jest mój <b>nos</b>.', examplePt: 'Esse é meu nariz.', pos: 'noun' },
+    { id: 'pl_body_7', deckId: 'pl_body', en: 'serce', pt: 'coração', exampleHtml: 'Moje <b>serce</b> bije szybko.', examplePt: 'Meu coração bate rápido.', pos: 'noun' },
+    { id: 'pl_body_8', deckId: 'pl_body', en: 'żołądek', pt: 'estômago', exampleHtml: 'Boli mnie <b>żołądek</b>.', examplePt: 'Minha barriga dói.', pos: 'noun' },
+    { id: 'pl_body_9', deckId: 'pl_body', en: 'plecy', pt: 'costas', exampleHtml: 'Bolą mnie <b>plecy</b>.', examplePt: 'Minhas costas doem.', pos: 'noun' },
+    { id: 'pl_body_10', deckId: 'pl_body', en: 'palec', pt: 'dedo', exampleHtml: 'To jest mój <b>palec</b>.', examplePt: 'Esse é meu dedo.', pos: 'noun' },
+    { id: 'pl_body_11', deckId: 'pl_body', en: 'włosy', pt: 'cabelo', exampleHtml: 'Mam ciemne <b>włosy</b>.', examplePt: 'Eu tenho cabelo escuro.', pos: 'noun' },
+    { id: 'pl_body_12', deckId: 'pl_body', en: 'twarz', pt: 'rosto', exampleHtml: 'To jest moja <b>twarz</b>.', examplePt: 'Esse é meu rosto.', pos: 'noun' },
+    { id: 'pl_body_13', deckId: 'pl_body', en: 'ząb', pt: 'dente', exampleHtml: 'Boli mnie <b>ząb</b>.', examplePt: 'Meu dente dói.', pos: 'noun' },
+  ],
+}
